bdfa009e1381e5f5473d40ef5e5e433883e5e417
vicent <vicheck88@gmail.com>
2021-10-11 01:49:21 +0900

    add momentum logic

--- Rscript/coinFunctionList.R
+++ Rscript/coinFunctionList.R
@@ -5,17 +5,20 @@ pkg = c('quantmod','jsonlite', 'stringr', 'logr',
         'httr')
 new.pkg = pkg[!(pkg %in% installed.packages()[, "Package"])]
 
-logDir<-"/home/pi/stockInfoCrawler/StockTradebot/log"
+#logDir<-"/home/pi/stockInfoCrawler/StockTradebot/log"
+logDir<-"C:/coinTestLog"
 
 if (length(new.pkg)) {
   install.packages(new.pkg, dependencies = TRUE)}
 sapply(pkg,library,character.only=T)
 
-getCoinMarketCapList<-function(){
+getCoinMarketCapList<-function(num){
   coinMarket_api_key<-"7a53f6d1-41fd-4658-836a-b59e6432f5cf"
+  url<-paste0("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=",num)
   h<-new_handle()
   handle_setheaders(h, .list=list("X-CMC_PRO_API_KEY"=coinMarket_api_key, Accepts="application/json"))
-  url<-"https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
+
+  
   r <- curl_fetch_memory(url, h)
   result<-fromJSON(rawToChar(r$content))$data
   priceList<-result$quote$USD
@@ -34,13 +37,20 @@ getUpbitCoinList<-function(){
   upbitCoinTable[,market:=str_replace(market,"KRW-","")]
   return(upbitCoinTable)
 }
-getTopNUpbitCoinList<-function(num){
-  coinMarketCapTable<-getCoinMarketCapList()
+getUpbitCoinListDetail<-function(num){
+  coinMarketCapTable<-getCoinMarketCapList(num)
+  #coinMarketCapTable$name<-toupper(coinMarketCapTable$name)
   upbitCoinTable<-getUpbitCoinList()
-  coinTable<-coinMarketCapTable[upbitCoinTable,on=c(symbol="market"),nomatch=0]
+  #upbitCoinTable$english_name<-toupper(upbitCoinTable$english_name)
+  coinTable<-coinMarketCapTable[upbitCoinTable,on=c(symbol="market")]
   coinTable<-coinTable[order(-market_cap)]
+  coinTable<-unique(coinTable,fromLast=FALSE,by="symbol")
+  coinTable<-na.omit(coinTable)
   coinTable<-subset(coinTable,select=-english_name)
-  return(coinTable[1:num,])
+  return(coinTable)
+}
+getTopNUpbitCoinList<-function(coinLimit, num){
+  return(getUpbitCoinListDetail(coinLimit)[1:num,])
 }
 createJwtToken<-function(query,random){
   upbitConfig<-fromJSON("./config.json")$upbit_key
@@ -86,6 +96,8 @@ getCurrentUpbitPrice<-function(coinList){
   return(priceList)
 }
 getCoinPriceHistory<-function(coinList,type,unit,count){
+  #type: minutes, days, weeks, months
+  #unit: 분봉(minutes), 상관없음(others)
   #coinList<-paste("KRW",coinList,sep="-")
   if(type=="minutes") {
     candle<-paste0("minutes/",unit)
@@ -103,6 +115,7 @@ getCoinPriceHistory<-function(coinList,type,unit,count){
   return(rbindlist(res))
 }
 getMomentumHistory<-function(coinList,candleType,unit,count,priceType,momentumPeriod){
+  #priceType: opening_price, high_price, low_price, trade_price
   priceList<-getCoinPriceHistory(coinList,candleType,unit,count)
   priceList<-subset(priceList,select=c("market","candle_date_time_kst",priceType))
   priceList[,prevPrice:=shift(get(priceType),momentumPeriod,NA,"lead"),by=market]
@@ -110,14 +123,15 @@ getMomentumHistory<-function(coinList,candleType,unit,count,priceType,momentumPe
   priceList[,momentum:=get(priceType)/prevPrice*100]
   return(subset(priceList,select=c("market","candle_date_time_kst","momentum")))
 }
-getEqualWeightBalanceDiff<-function(num){
+getUpbitCoinMomentum<-function(candleType,unit,momentumPeriod, coinList){
+  coinList<-paste("KRW",coinList$symbol,sep="-")
+  momentum<-getMomentumHistory(coinList,candleType,unit,momentumPeriod+1,"trade_price",momentumPeriod)
+  return(momentum)
+}
+
+getCurrentBalance<-function(){
   logPath<-paste0(logDir,"coinLog.",Sys.Date(),".log")
   log_open(logPath)
-  
-  log_print(paste0("TOP ",num,"COIN LIST:"))
-  topNCoinList<-getTopNUpbitCoinList(num)
-  log_print(topNCoinList)
-  topNCoinList[,market:=paste0("KRW-",symbol)]
   
   log_print("CURRENT BALANCE")
   krwCoinList<-getUpbitCoinList()
@@ -129,7 +143,6 @@ getEqualWeightBalanceDiff<-function(num){
   log_print(balanceList)
   
   balanceList[,market:=paste0("KRW-",currency)]
-  topNCoinList<-topNCoinList[,.(name,symbol,price)]
   log_print("CURRENT COIN PRICE")
   price=getCurrentUpbitPrice(balanceList$market[-1])
   price<-rbind(price,as.list(c("KRW-KRW",1)))
@@ -137,38 +150,73 @@ getEqualWeightBalanceDiff<-function(num){
   balanceList<-merge(balanceList,price,by.x="market",by.y="market")
   balanceList[,trade_price:=as.double(trade_price)]
   curBalanceList<-balanceList[,.(market,currency,balance=balance*trade_price,curvolume=balance)]
-  totalBalance<-curBalanceList[,sum(balance)]
-  topNCoinList$targetbalance<-totalBalance/num
-  
-  joinList<-merge(topNCoinList,curBalanceList,by.x="symbol",by.y="currency",all=TRUE)
-  joinList<-joinList[symbol!="KRW"]
-  joinList[,market:=paste0("KRW-",symbol)]
-  minimumOrder<-getMinimumOrderUnit(joinList$market)
-  joinList<-merge(joinList,minimumOrder,by.x="market",by.y="market",all=TRUE)
-  joinList[is.na(balance)]$balance<-0
-  joinList[is.na(targetbalance)]$targetbalance<-0
-  joinList[,diff:=targetbalance-balance]
-  
-  joinList[diff<0][diff>ask_min]$targetbalance<-joinList[diff<0][diff>ask_min]$balance
-  joinList[diff>0][diff<bid_min]$targetbalance<-joinList[diff>0][diff<bid_min]$balance
-  joinList[,diff:=targetbalance-balance]
-  remainedBalance<-totalBalance-joinList[diff==0][,sum(balance)]
-  joinList[diff!=0][targetbalance>0]$targetbalance<-remainedBalance/NROW(joinList[diff!=0][targetbalance>0])
-  joinList[,diff:=targetbalance-balance]
-  
-  joinList<-joinList[,.(symbol,diff,curvolume,price)]
-  names(joinList)<-c("market","buyamount","currentvolume","price")
-  log_print("COIN LIST FOR ORDER")
-  log_print(joinList)
-  log_close()
-  return(joinList)
+  return(curBalanceList)
 }
-rebalanceWeight<-function(table){
+
+getMomentumBalance<-function(coinList,num,limitRatio,type,momentumList){
+  momentumList[,symbol:=sapply(strsplit(market,"-"),function(x)x[2])]
+  momentumList <- coinList[momentumList,on=c("symbol"),nomatch=0]
+  momentumList<-na.omit(momentumList)
+  momentumList<-momentumList[order(-momentum)]
+  momentumList<-momentumList[,.(symbol,market,market_cap)]
+  if(type=="MARKET"){
+    momentumList[,ratio:=market_cap/sum(market_cap)*limitRatio]  
+  } else if(type=="EQUAL"){
+    momentumList[,ratio:=1/num*limitRatio]  
+  }
+  return(momentumList[1:num,])
+}
+
+getIndexBalance<-function(coinList, limitRatio, type){
+  coinList[,market:=paste0("KRW-",symbol)]
+  coinList<-coinList[,.(symbol,market,market_cap)]
+  
+  num<-NROW(coinList)
+  if(type=="MARKET"){
+    coinList[,ratio:=market_cap/sum(market_cap)*limitRatio]  
+  } else if(type=="EQUAL"){
+    coinList[,ratio:=1/num*limitRatio]  
+  }
+  return(coinList)
+}
+createOrderTable<-function(table,currentBalance){
+  totalBalance<-sum(currentBalance$balance)
+  balanceCombinedTable<-merge(table,currentBalance,by="market",all=TRUE)
+  balanceCombinedTable[,totalBalance:=totalBalance]
+  balanceCombinedTable<-balanceCombinedTable[market!="KRW-KRW"]
+  balanceCombinedTable[,targetBalance:=totalBalance*ratio]
+  
+  minimumOrder<-getMinimumOrderUnit(balanceCombinedTable$market)
+  balanceCombinedTable<-merge(balanceCombinedTable,minimumOrder,by.x="market",by.y="market",all=TRUE)
+  
+  balanceCombinedTable[is.na(balance)]$balance<-0
+  balanceCombinedTable[is.na(targetBalance)]$targetBalance<-0
+  balanceCombinedTable[is.na(ratio)]$ratio<-0
+  balanceCombinedTable[is.na(curvolume)]$curvolume<-0
+  balanceCombinedTable[,diff:=targetBalance-balance]
+  
+  balanceCombinedTable[diff<0][diff>ask_min]$targetBalance<-balanceCombinedTable[diff<0][diff>ask_min]$balance
+  balanceCombinedTable[diff>0][diff<bid_min]$targetBalance<-balanceCombinedTable[diff>0][diff<bid_min]$balance
+  balanceCombinedTable[,diff:=targetBalance-balance]
+  
+  
+  remainedBalance<-totalBalance-balanceCombinedTable[diff==0][,sum(balance)]
+  
+  if(remainedBalance!=totalBalance){
+    balanceCombinedTable[diff!=0]$targetBalance<-balanceCombinedTable[diff!=0][,ratio]*remainedBalance  
+    balanceCombinedTable[,diff:=targetBalance-balance]
+  }
+  
+  balanceCombinedTable<-balanceCombinedTable[,.(market,diff,curvolume)]
+  names(balanceCombinedTable)<-c("market","buyamount","currentvolume")
+  return(balanceCombinedTable)
+}
+
+
+rebalanceTable<-function(table){
   logPath<-paste0(logDir,"coinLog.",Sys.Date(),".log")
   log_open(logPath)
-  
   table<-table[buyamount!=0]
-  table[,market:=paste("KRW",market,sep="-")]
   table$ord_type<-'limit'
   table$side<-'bid'
   table[buyamount<0]$side<-'ask'
@@ -176,24 +224,24 @@ rebalanceWeight<-function(table){
   table[,price:=getCurrentUpbitPrice(table$market)$trade_price]
   table[,volume:=buyamount/price]
   table[side=="ask"][currentvolume<volume]$volume<-table[side=="ask"][currentvolume<volume]$currentvolume
-  table<-subset(table,select=c("market","side","volume","price","ord_type"))
+ 
+   log_open()
   
-  log_print("FINAL ORDER LIST")
+  table<-subset(table,select=c("market","side","volume","price","ord_type"))
+  log_print("Final Table List")
   log_print(table)
+  log_close()
   
   if(NROW(table[side=="ask"])>0){
     orderCoin(table[side=="ask"])
     Sys.sleep(5)
   }
   orderCoin(table[side=="bid"])
-  log_close()
 }
-getOrderList<-function(status){
-  query<-paste0("state=",status)
-  url<-"https://api.upbit.com/v1/orders"
-}
+
 orderCoin<-function(order){
-  
+  logPath<-paste0(logDir,"coinLog.",Sys.Date(),".log")
+  log_open(logPath)
   query<-paste0("market=",order$market,"&side=",order$side,"&volume=",order$volume,"&price=",order$price,"&ord_type=",order$ord_type)
   tokenList<-sapply(query,function(x) createJwtToken(x,runif(1,1000,33553))) 
   url<-"https://api.upbit.com/v1/orders"
@@ -205,6 +253,7 @@ orderCoin<-function(order){
     Sys.sleep(0.3)
   }
 }
+
 getMinimumOrderUnit<-function(coinList){
   table<-NULL
   query<-paste0("market=",coinList)
